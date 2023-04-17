b939388ac74769e6aafee42db2bd5bfcc2c1f433
Douglas Leal <douglas.leal@live.com>
2023-04-17 13:33:25 -0300

    ContactList page criado

--- lib/pages/contacts_list.dart
+++ lib/pages/contacts_list.dart
@@ -1,10 +1,102 @@
+import 'package:api/database/dao/contact_dao.dart';
+import 'package:api/models/contact.dart';
+import 'package:api/pages/contact_form.dart';
+import 'package:api/pages/transaction_form.dart';
 import 'package:flutter/material.dart';
 
-class ContactsList extends StatelessWidget {
+import '../components/progress.dart';
+
+class ContactsList extends StatefulWidget {
   const ContactsList({Key? key}) : super(key: key);
 
   @override
+  State<ContactsList> createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+  final ContactDao _dao = ContactDao();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Transfer"),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const ContactForm(),
+            ),
+          ).then((value) => setState(() {}));
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: [],
+        future: _dao.findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return const Progress();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact>? contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts![index];
+                  return _ContactItem(
+                    contact,
+                    onClick: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TransactionForm(),
+                        ),
+                      );
+                    },
+                  );
+                },
+                itemCount: contacts != null ? contacts.length : 0,
+              );
+              break;
+          }
+          return const Text('Unknown error');
+        },
+      )
+    );
   }
 }
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+  final Function onClick;
+
+  const _ContactItem(
+      this.contact, {
+        required this.onClick,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () => onClick(),
+        title: Text(
+          contact.name,
+          style: const TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: const TextStyle(
+            fontSize: 16.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
